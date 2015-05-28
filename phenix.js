36ed6597d9a1b89e1a4b670e71e997d9ebb097c3
James Liu <jamesleu96@gmail.com>
2015-05-28 17:50:56 +0800

    v0.3 mask

--- phenix.js
+++ phenix.js
@@ -1,5 +1,5 @@
 /*!
- * Phenix.js v0.2
+ * Phenix.js v0.3
  * A dummy pop-up box.
  * github.com/jamesliu96/phenix.js
  *
@@ -61,6 +61,9 @@
 
 	Phenix.element = Phenix.dom.createElement('div');
 	Phenix.element.setAttribute('id', 'phenix');
+	Phenix.mask = Phenix.dom.createElement('div');
+	Phenix.mask.setAttribute('id', 'phenix-mask');
+	Phenix.mask.style.cssText = 'position:fixed;left:0;top:0;width:100%;height:100%;padding:0;margin:0;z-index:99998;background-color:rgba(0,0,0,0.4);';
 
 	Phenix.defaults = {};
 	Phenix.defaults.config = {
@@ -71,7 +74,7 @@
 			color: "#36b14a",
 			face: ":)",
 			title: "Welcome to Phenix",
-			description: "Copyright (C) 2015 James Liu"
+			description: "Made with ❤ by James Liu"
 		}
 	};
 	Phenix.defaults.style = {
@@ -104,13 +107,17 @@
 	};
 
 	var _opacity = function(opacity) {
-		var element = Phenix.element;
+		var element = Phenix.element,
+			mask = Phenix.mask;
 		element.filter ? element.style.filter = 'alpha(opacity=' + opacity + ')' : element.style.opacity = opacity / 100;
+		mask.filter ? mask.style.filter = 'alpha(opacity=' + opacity + ')' : mask.style.opacity = opacity / 100;
 	}
 
 	Phenix.show = function(callback) {
-		var element = Phenix.element;
+		var element = Phenix.element,
+			mask = Phenix.mask;
 		_DOMcreate(element);
+		_DOMcreate(mask);
 		var opacity = 0;
 		(function(){
 			_opacity(opacity);
@@ -124,8 +131,19 @@
 	};
 
 	Phenix.hide = function(callback) {
-		var element = Phenix.element;
-		_DOMdestroy(element);
+		var element = Phenix.element,
+			mask = Phenix.mask;
+		var opacity = 100;
+		(function(){
+			_opacity(opacity);
+			opacity -= 15;
+			if (opacity >= 0) {
+				setTimeout(arguments.callee, 20);
+			} else {
+				_DOMdestroy(element);
+				_DOMdestroy(mask);
+			}
+		})();
 		(typeof callback === "function") && callback();
 		return Phenix;
 	};
